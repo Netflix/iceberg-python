c9cf36de244a4d82ff7f4e2b2f2e1ff4afe4710f
Fokko Driesprong <fokko@apache.org>
2025-03-25 20:39:08 +0100

    REST: Delegate parsing to Pydantic (#1847)
    # Rationale for this change
    
    Right now we deserialize the JSON into a dict, which is then passed into
    the Pydantic model. It is better to fully delegate this to pydantic
    because it is probably faster, and we can detect when models are created
    from json or from Python dicts.
    
    Required by https://github.com/apache/iceberg-python/pull/1770
    
    This is also a recommendation by Pydantic itself:
    https://docs.pydantic.dev/latest/concepts/performance/#in-general-use-model_validate_json-not-model_validatejsonloads
    
    # Are these changes tested?
    
    Existing tests
    
    # Are there any user-facing changes?
    
    No
    
    <!-- In the case of user-facing changes, please add the changelog label.
    -->

--- pyiceberg/catalog/rest.py
+++ pyiceberg/catalog/rest.py
@@ -378,7 +378,7 @@ class RestCatalog(Catalog):
         except HTTPError as exc:
             self._handle_non_200_response(exc, {400: OAuthError, 401: OAuthError})
 
-        return TokenResponse(**response.json()).access_token
+        return TokenResponse.model_validate_json(response.text).access_token
 
     def _fetch_config(self) -> None:
         params = {}
@@ -391,7 +391,7 @@ class RestCatalog(Catalog):
             response.raise_for_status()
         except HTTPError as exc:
             self._handle_non_200_response(exc, {})
-        config_response = ConfigResponse(**response.json())
+        config_response = ConfigResponse.model_validate_json(response.text)
 
         config = config_response.defaults
         config.update(self.properties)
@@ -451,14 +451,14 @@ class RestCatalog(Catalog):
         try:
             if exception == OAuthError:
                 # The OAuthErrorResponse has a different format
-                error = OAuthErrorResponse(**exc.response.json())
+                error = OAuthErrorResponse.model_validate_json(exc.response.text)
                 response = str(error.error)
                 if description := error.error_description:
                     response += f": {description}"
                 if uri := error.error_uri:
                     response += f" ({uri})"
             else:
-                error = ErrorResponse(**exc.response.json()).error
+                error = ErrorResponse.model_validate_json(exc.response.text).error
                 response = f"{error.type}: {error.message}"
         except JSONDecodeError:
             # In the case we don't have a proper response
@@ -596,7 +596,7 @@ class RestCatalog(Catalog):
             response.raise_for_status()
         except HTTPError as exc:
             self._handle_non_200_response(exc, {409: TableAlreadyExistsError})
-        return TableResponse(**response.json())
+        return TableResponse.model_validate_json(response.text)
 
     @retry(**_RETRY_ARGS)
     def create_table(
@@ -670,7 +670,7 @@ class RestCatalog(Catalog):
         except HTTPError as exc:
             self._handle_non_200_response(exc, {409: TableAlreadyExistsError})
 
-        table_response = TableResponse(**response.json())
+        table_response = TableResponse.model_validate_json(response.text)
         return self._response_to_table(self.identifier_to_tuple(identifier), table_response)
 
     @retry(**_RETRY_ARGS)
@@ -682,7 +682,7 @@ class RestCatalog(Catalog):
             response.raise_for_status()
         except HTTPError as exc:
             self._handle_non_200_response(exc, {404: NoSuchNamespaceError})
-        return [(*table.namespace, table.name) for table in ListTablesResponse(**response.json()).identifiers]
+        return [(*table.namespace, table.name) for table in ListTablesResponse.model_validate_json(response.text).identifiers]
 
     @retry(**_RETRY_ARGS)
     def load_table(self, identifier: Union[str, Identifier]) -> Table:
@@ -692,7 +692,7 @@ class RestCatalog(Catalog):
         except HTTPError as exc:
             self._handle_non_200_response(exc, {404: NoSuchTableError})
 
-        table_response = TableResponse(**response.json())
+        table_response = TableResponse.model_validate_json(response.text)
         return self._response_to_table(self.identifier_to_tuple(identifier), table_response)
 
     @retry(**_RETRY_ARGS)
@@ -743,7 +743,7 @@ class RestCatalog(Catalog):
             response.raise_for_status()
         except HTTPError as exc:
             self._handle_non_200_response(exc, {404: NoSuchNamespaceError})
-        return [(*view.namespace, view.name) for view in ListViewsResponse(**response.json()).identifiers]
+        return [(*view.namespace, view.name) for view in ListViewsResponse.model_validate_json(response.text).identifiers]
 
     @retry(**_RETRY_ARGS)
     def commit_table(
@@ -789,7 +789,7 @@ class RestCatalog(Catalog):
                     504: CommitStateUnknownException,
                 },
             )
-        return CommitTableResponse(**response.json())
+        return CommitTableResponse.model_validate_json(response.text)
 
     @retry(**_RETRY_ARGS)
     def create_namespace(self, namespace: Union[str, Identifier], properties: Properties = EMPTY_DICT) -> None:
@@ -826,7 +826,7 @@ class RestCatalog(Catalog):
         except HTTPError as exc:
             self._handle_non_200_response(exc, {})
 
-        return ListNamespaceResponse(**response.json()).namespaces
+        return ListNamespaceResponse.model_validate_json(response.text).namespaces
 
     @retry(**_RETRY_ARGS)
     def load_namespace_properties(self, namespace: Union[str, Identifier]) -> Properties:
@@ -838,7 +838,7 @@ class RestCatalog(Catalog):
         except HTTPError as exc:
             self._handle_non_200_response(exc, {404: NoSuchNamespaceError})
 
-        return NamespaceResponse(**response.json()).properties
+        return NamespaceResponse.model_validate_json(response.text).properties
 
     @retry(**_RETRY_ARGS)
     def update_namespace_properties(
@@ -852,7 +852,7 @@ class RestCatalog(Catalog):
             response.raise_for_status()
         except HTTPError as exc:
             self._handle_non_200_response(exc, {404: NoSuchNamespaceError})
-        parsed_response = UpdateNamespacePropertiesResponse(**response.json())
+        parsed_response = UpdateNamespacePropertiesResponse.model_validate_json(response.text)
         return PropertiesUpdateSummary(
             removed=parsed_response.removed,
             updated=parsed_response.updated,
